0804b93504f13ab1b259bf5115bc8f5330dd72f9
thederpykrafter <thederpykrafter@gmail.com>
2025-05-25 10:50:32 -0400

    nerdfont icon picker

---
+++ lua/plugins/nerdy.lua
@@ -0,0 +1,10 @@
+return {
+  '2kabhishek/nerdy.nvim',
+  -- dependencies = {
+  --     '',
+  -- },
+  cmd = 'Nerdy',
+  keys = {
+    { '<leader>n', '<cmd>Nerdy<cr>', desc = 'NerdFont Icon Picker' },
+  },
+}
