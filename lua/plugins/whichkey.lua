8d091d35245e2f9765bd59344e42097e8f0a7e92
thederpykrafter <thederpykrafter@gmail.com>
2025-06-24 19:37:27 -0400

    add key groups

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -5,14 +5,18 @@ return {
     require('which-key').setup()
     require('which-key').add({
       -- Lazy Prefix
-      { '<leader>d', group = 'Diagnostic' },
+      { '<leader>d', group = 'Document' },
       { '<leader>d_', hidden = true },
       { '<leader>g', group = 'Git' },
       { '<leader>g_', hidden = true },
       { '<leader>l', group = 'Lazy' },
       { '<leader>l_', hidden = true },
+      { '<leader>n', group = 'Icon/Symbol Pickers' },
+      { '<leader>n_', hidden = true },
       { '<leader>s', group = 'Search' },
       { '<leader>s_', hidden = true },
+      { '<leader>t', group = 'Toggle' },
+      { '<leader>t_', hidden = true },
     })
   end,
 }
