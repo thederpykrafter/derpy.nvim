1d5f9a34a206cdc7fc631fcda40d0cf667e35b7d
thederpykrafter <thederpykrafter@gmail.com>
2025-06-30 01:33:16 -0400

    cleaner smear cursor

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -4,7 +4,13 @@ return {
   opts = {
     cursor_color = '#d2d2d2',
     legacy_computing_symbols_support = true,
+    transparent_bg_fallback_color = '#303030',
     hide_target_hack = true,
     never_draw_over_target = true,
+    -- remove trail
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    damping = 0.67,
+    matrix_pixel_threshold = 0.5,
   },
 }
