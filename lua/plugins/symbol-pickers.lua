bd2f7712c146302cc5076ef369af9952cd1b4792
thederpykrafter <thederpykrafter@gmail.com>
2025-07-21 17:31:22 -0400

    update Nerdy

--- lua/plugins/symbol-pickers.lua
+++ lua/plugins/symbol-pickers.lua
@@ -8,6 +8,11 @@ return {
     keys = {
       { '<leader>nn', '<cmd>Nerdy<cr>', desc = 'NerdFont Icon Picker' },
     },
+    opts = {
+      max_recents = 30, -- Configure recent icons limit
+      add_default_keybindings = true, -- Add default keybindings
+      use_new_command = true, -- Enable new command system
+    },
   },
   {
     'nvim-telescope/telescope-symbols.nvim',
