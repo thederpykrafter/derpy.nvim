90d1f3c0f0adfcbe9b21e53d985018f3ac4838c0
thederpykrafter <thederpykrafter@gmail.com>
2025-07-31 08:10:45 -0400

    changes idk

---
+++ lua/plugins/md-preview.lua
@@ -0,0 +1,12 @@
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+  dependencies = {
+    'saghen/blink.cmp',
+  },
+  opts = {
+    experimental = {
+      check_rtp_message = false,
+    },
+  },
+}
